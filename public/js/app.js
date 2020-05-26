aae2008db922f61c7af5867e0ff39cc4e0472d44
Dhirajbhujbal <dhiraj.bhujbal1994@gmail.com>
2020-05-26 18:13:18 +0530

    Stylesheet applied to website

--- public/js/app.js
+++ public/js/app.js
@@ -8,16 +8,23 @@ setTimeout( () => {
     const btnId = document.getElementById('searchId');
     const forecastInfo1 = document.querySelector('#forecastInfo1');
     const forecastInfo2 = document.querySelector('#forecastInfo2');
+    const rportCardHeader = document.querySelector('#rportCardHeader');
+
+    
 
     btnId.addEventListener('click', (e) => {
         const searchValue = document.getElementsByClassName('inputBox')[0].value
         forecastInfo1.textContent = 'Loading.......';
-        fetch(`http://localhost:3000/weather?address=${searchValue}`).then((res) => {
+        forecastInfo2.textContent = '';
+        fetch(`/weather?address=${searchValue}`).then((res) => {
             return res.json();
         }).then((res) => {
             if(res.error) {
                 forecastInfo1.textContent = res.error;
+                rportCardHeader.style.display = 'none';
+                forecastInfo2.textContent = ''
             } else {
+                rportCardHeader.style.display = 'block';
                 forecastInfo1.textContent = res.foreCastData;
                 forecastInfo2.textContent = res.locationName;
                 // console.log(res)
